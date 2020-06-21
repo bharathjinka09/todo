2afb5e36f260e2a6c75f05d3efb25e6ef9dda109
bharathjinka09 <bharathjinka09@gmail.com>
2020-06-21 19:00:19 +0530

    Updates

---
+++ precache-manifest.8631d5852ff82911468a6d3069af6a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3f02a01c586c34165c0cb3f64e3e2c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b7720a9208c6c0ba73ae",
+    "url": "/todo/static/css/main.37e759d6.chunk.css"
+  },
+  {
+    "revision": "e0f0dc00ae6ae6f602c3",
+    "url": "/todo/static/js/2.e5f8debb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.e5f8debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7720a9208c6c0ba73ae",
+    "url": "/todo/static/js/main.b4577bb5.chunk.js"
+  },
+  {
+    "revision": "6df291ff9329873806f0",
+    "url": "/todo/static/js/runtime-main.8a8caff8.js"
+  }
+]);
